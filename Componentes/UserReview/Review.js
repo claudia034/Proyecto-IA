5cf3e37b6d25aa46a280f57963817a2b1e43e92a
luciale <lucialecabrerao@gmail.com>
2024-05-30 11:31:34 -0600

    add and get reviews

--- Componentes/UserReview/Review.js
+++ Componentes/UserReview/Review.js
@@ -11,4 +11,56 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         window.location.href = 'index.html';
     }
+
+    document.getElementById('sendReviewButton').addEventListener('click', function() {
+        document.getElementById('popup').style.display = 'flex';
+    });
+
+    document.getElementById('closeButton').addEventListener('click', function() {
+        document.getElementById('popup').style.display = 'none';
+    });
+
+    document.getElementById('submitReviewButton').addEventListener('click', function() {
+        const reviewText = document.getElementById('reviewText').value;
+        const publisher = document.getElementById('publisher').value;
+        const rottenLink = document.getElementById('rottenLink').value;
+        const user = document.getElementById('user').value;
+        const movieTitle = title;
+
+        if (reviewText && publisher && rottenLink && user) {
+            fetch('http://127.0.0.1:5000/predict', { // Asegúrate de que esta URL sea correcta
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    review: reviewText,
+                    publisher: publisher,
+                    rotten_link: rottenLink,
+                    user: user,
+                    movie_title: movieTitle
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.classification) {
+                    alert('Review submitted successfully! Classification: ' + data.classification);
+                } else {
+                    alert('Failed to submit review: ' + data.message);
+                }
+                document.getElementById('popup').style.display = 'none';
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('An error occurred while submitting the review.');
+            });
+        } else {
+            alert('Please fill in all fields.');
+        }
+    });
 });
